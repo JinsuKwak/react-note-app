4ab9079468423cc68bf9c1cff64b6d2470e8b77a
Jinsu Kwak <jinsukwak2001@gmail.com>
2023-09-16 23:22:28 -0600

    add error page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Sidebar />
-        <div className="app_container">
+        <div className="app__container">
           <Routes>
             <Route path="/" element={<AllNotes />} />
             <Route path="/archive" element={<ArchiveNotes />} />
--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container } from "./ErrorPage.styles";
+import errorImg from "../../assets/errorImg.png";
+import { ButtonFill } from "../../styles/styles";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <div className="error__img">
+        <img src={errorImg} alt="pageNotFound" />
+      </div>
+      <div className="error__text">
+        <h1>404</h1>
+        <div>Page Not Found</div>
+        <ButtonFill onClick={() => navigate("/")}>
+          <span>Back to Main Page</span>
+        </ButtonFill>
+      </div>
+    </Container>
+  );
 };
 
 export default ErrorPage;
