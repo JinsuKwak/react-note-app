23fe788d4e127cdbd828e2426ec11f0d68d361ba
Jinsu Kwak <jinsukwak2001@gmail.com>
2023-09-17 23:08:44 -0600

    add trash note page

--- src/pages/TrashNotes/TrashNotes.tsx
+++ src/pages/TrashNotes/TrashNotes.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { MainWrapper } from "../../components";
+import { useAppSelector } from "../../hooks/redux";
+import { Container, EmptyMsgBox } from "../../styles/styles";
 
-const TrashNote = () => {
-  return <div>TrashNote</div>;
+const TrashNotes = () => {
+  const { trashNotes } = useAppSelector((state) => state.notesList);
+
+  return (
+    <Container>
+      {trashNotes.length === 0 ? (
+        <EmptyMsgBox>No Notes...</EmptyMsgBox>
+      ) : (
+        <MainWrapper notes={trashNotes} type="trash" />
+      )}
+    </Container>
+  );
 };
 
-export default TrashNote;
+export default TrashNotes;
